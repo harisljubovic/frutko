ff927be48b09bb6aa7b9a1cd884dca9a6c6a55dd
Haris <harisljubovic@gmail.com>
2024-05-17 00:50:24 +0200

    17.05.2024

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let navBar = document.querySelector("#navBar");
+let dropdownMenu = document.querySelector("#dropdownMenu");
 
 window.onscroll = () => {
   navBarScroll();
@@ -7,7 +8,9 @@ window.onscroll = () => {
 function navBarScroll() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     navBar.classList.add("navBarScroll");
+    dropdownMenu.classList.remove("dropdownMenuTransparent");
   } else {
     navBar.classList.remove("navBarScroll");
+    dropdownMenu.classList.add("dropdownMenuTransparent");
   }
 }
